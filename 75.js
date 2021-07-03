d6ce2cf1df9c9dd0ddf149d2b3f834f2314a8e60
shreyasghuge <sghuge@vibrenthealth.com>
2021-07-03 20:16:49 +0530

    Problem#75: Learnt Dutch National Flag algorithm

---
+++ 75.js
@@ -0,0 +1,47 @@
+/**
+ * This solution is solved using the Dutch National Flag Algorithm.
+ * 
+ * Conditions of this algorithm:
+ *   elements(array[0...low]) = 0
+ *   elements(array[low...high]) = 1
+ *   elements(array[high...n]) = 2
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * 
+ */
+let input_array = [2, 0, 2, 1, 1, 0]
+
+function main() {
+  let low, mid, high;
+  low = mid = 0;
+  high = input_array.length - 1;
+
+  while (mid <= high) {
+    switch (input_array[mid]) {
+      case 0:
+        swap(low, mid);
+        low++;
+        mid++;
+        break;
+
+      case 1:
+        mid++;
+        break;
+
+      case 2:
+        swap(mid, high);
+        high--;
+        break;
+    }
+  }
+  console.log(input_array);
+}
+
+function swap(i, j) {
+  let temp = input_array[i];
+  input_array[i] = input_array[j];
+  input_array[j] = temp;
+}
+
+main()
