56a388fd967db11139d97359ab1186e0f3b2c936
shreyasghuge <sghuge@vibrenthealth.com>
2021-07-03 20:57:48 +0530

    Problem#287: Learnt Set in js

---
+++ Medium/287.js
@@ -0,0 +1,27 @@
+/**
+ * In this solution, I learnt using a Set in js.
+ * Set has apis such as: .add(), .delete(), .clear(), .has(), .size(), etc
+ * 
+ * Time Complexity: O(n)
+ * Space Complexity: O(n - 1) => O(n)
+ */
+
+let input_array = [1, 3, 4, 2, 2]
+
+function findDuplicate() {
+  let mySet = new Set();
+
+  for (let i = 0; i < input_array.length; i++) {
+    if (mySet.has(input_array[i])) {
+      return input_array[i];
+    } else {
+      mySet.add(input_array[i]);
+    }
+  }
+};
+
+function main() {
+  console.log(findDuplicate());
+}
+
+main()
